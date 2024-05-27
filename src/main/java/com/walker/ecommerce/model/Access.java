eea99fe789eacdccb0eba3d52919d1fde3ceb0f8
Walker Negrão <c0662240@vale.com>
2024-05-27 13:15:55 -0300

    Classe de entidade Acess criada

---
+++ src/main/java/com/walker/ecommerce/model/Access.java
@@ -0,0 +1,61 @@
+package com.walker.ecommerce.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_access")
+@SequenceGenerator(name = "seq_access",sequenceName = "seq_access",initialValue = 1,allocationSize = 1)
+public class Access implements GrantedAuthority { //Acesso
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_access")
+    private Long id;
+    @Column(nullable = false)
+    private String description; //descrição Exemplos: ROLE_ADMIN, ROLE_
+
+    public Access() {
+    }
+
+    public Access(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Access access = (Access) o;
+        return Objects.equals(id, access.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+
+}
