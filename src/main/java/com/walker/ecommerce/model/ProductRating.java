a0f007c38fdf57c278f6dbd9558b92d197ee3686
Walker Negrão <c0662240@vale.com>
2024-06-10 13:58:49 -0300

    Criação da classe de entidade ProductRating

---
+++ src/main/java/com/walker/ecommerce/model/ProductRating.java
@@ -0,0 +1,89 @@
+package com.walker.ecommerce.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product_rating")
+@SequenceGenerator(name = "seq_product_rating",sequenceName = "seq_product_rating",initialValue = 1,allocationSize = 1)
+public class ProductRating implements Serializable { //AvalicaoProduto
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_product_rating")
+    private Long id;
+    private Integer score; //nota
+    private String justification; //justificativa
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "person_id",nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "person_fk"))
+    private Person person; //pessoa
+    @ManyToOne
+    @JoinColumn(name = "product_id",nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "product_fk"))
+    private Product product; //produto
+
+    public ProductRating() {
+    }
+
+    public ProductRating(Long id, Integer score, String justification, Person person, Product product) {
+        this.id = id;
+        this.score = score;
+        this.justification = justification;
+        this.person = person;
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getJustification() {
+        return justification;
+    }
+
+    public void setJustification(String justification) {
+        this.justification = justification;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductRating that = (ProductRating) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/walker/ecommerce/model/SaleItem.java
+++ src/main/java/com/walker/ecommerce/model/SaleItem.java
@@ -2,6 +2,7 @@ package com.walker.ecommerce.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_sale_item")
@@ -10,13 +11,68 @@ public class SaleItem implements Serializable { //ItemVendaLoja
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_sale_item")
     private Long id;
-    private Double quantity;
+    private Double quantity; //quantidade
 
 
     @ManyToOne
     @JoinColumn(name = "product_id",nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "product_fk"))
-    private Product product;
+    private Product product; //produto
     @ManyToOne
     @JoinColumn(name = "sale_id",nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "sale_fk"))
-    private Sale sale;
+    private Sale sale; //venda
+
+    public SaleItem() {
+    }
+
+    public SaleItem(Long id, Double quantity, Product product, Sale sale) {
+        this.id = id;
+        this.quantity = quantity;
+        this.product = product;
+        this.sale = sale;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Sale getSale() {
+        return sale;
+    }
+
+    public void setSale(Sale sale) {
+        this.sale = sale;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SaleItem saleItem = (SaleItem) o;
+        return Objects.equals(id, saleItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
