afe6753c95b29977baa62bf8c236466dde5010a2
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-07-08 18:11:55 -0300

    Criado o teste de verificação de cadastro de acesso utilizando JUnit

--- src/test/java/com/walker/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/com/walker/ecommerce/ECommerceApplicationTests.java
@@ -3,19 +3,29 @@ package com.walker.ecommerce;
 import com.walker.ecommerce.controller.AccessController;
 import com.walker.ecommerce.model.Access;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = ECommerceApplication.class)
-class ECommerceApplicationTests {
+public class ECommerceApplicationTests {
+
 	@Autowired
 	private AccessController accessController;
 
 	@Test
+	@DisplayName("Testa se a API registra um novo acesso")
 	public void testRegistrationAccess() {
 		Access access = new Access();
-		access.setDescription("ROLE_ADMIN");
-		accessController.saveAccess(access);
-	}
+		access.setDescription("ROLE_ADMIN_TEST_JUNIT");
+		ResponseEntity<Access> responseEntity = accessController.registerAccess(access);
+		assertNotNull(responseEntity); // Verifica se o responseEntity não é nulo
 
+		Access registerAccess = responseEntity.getBody();
+		assertTrue(registerAccess.getId() > 0);
+		assertEquals("ROLE_ADMIN_TEST_JUNIT",registerAccess.getAuthority());
+	}
 }
